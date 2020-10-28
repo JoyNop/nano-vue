01dcd0f795670d8cf0399c1fd0ec0e8db0a28c0d
HanRui <mail@hr.im>
2020-10-28 10:06:34 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,23 @@ import store from "./store";
 import "@/assets/css/base.css";
 //main style
 import "@/assets/css/main.less";
+import "@vant/touch-emulator"; 
 
-//install globalComponents
+
+//install Global Components
 import BaseComponents from "@/components/base";
-
+//install Custom Directives
 import BaseDirective from '@/directives'
-import "@vant/touch-emulator";
- 
+
+
 import axios from "axios";
 import VueAxios from "vue-axios";
 
 const app = createApp(App);
 
-//use globalComponents
+//use Global Components
 app.use(BaseComponents)
+//use Custom Directives
 app.use(BaseDirective)
 
 app.use(store);
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,19 @@
 // vue.config.js
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/nano-vue/" : "./",
+  outputDir: "docs",
   css: {
     loaderOptions: {
       less: {
         // 若使用 less-loader@5，请移除 lessOptions 这一级，直接配置选项。
-     
-          modifyVars: {
-            // 直接覆盖变量
-            'text-color': '#111',
-            'border-color': '#eee',
-            // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
-            // hack: `true; @import "your-less-file-path.less";`,
-          },
-        
+
+        modifyVars: {
+          // 直接覆盖变量
+          "text-color": "#111",
+          "border-color": "#eee",
+          // 或者可以通过 less 文件覆盖（文件路径为绝对路径）
+          // hack: `true; @import "your-less-file-path.less";`,
+        },
       },
     },
   },
